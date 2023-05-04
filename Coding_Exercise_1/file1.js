987689f363930e07a115fc605cf593941ef0b8f5
Himanshu Khobragade <45400227+lokmitra56@users.noreply.github.com>
2023-05-04 16:10:15 -0700

    Create file1.js
    Adding Image files

---
+++ Coding_Exercise_1/file1.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// don't change the Component name "App"
+export default function App() {
+    return (
+        <div>
+            <h1>Exercise done!</h1>
+        </div>
+    );
+}
